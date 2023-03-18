fdaebe0e10c5750e1219863f4ec8b3123f9fccf9
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-18 19:24:27 -0300

    Correções de bugs e relacionados
    -Correção um bug que fazia com que as imagens animadas dos pokémons após id:650 não aparececem por falta de dados na pokéAPI, deixando assim todos os pokémons após o id:650 estáticos;
    -Correção de bug que fazia com que pokémons pós id:722 ficassem MUITO pequenos;
    -Correção de outros bugs relacionados a tamanhos de pokémon;
    -Correção de bug que não buscava pokémons caso não estivesse com todas as letras minúsculas; 
    -CORREÇÃO DESFEITA: Correção de bug que fazia com que alguns pokémons ultrapassassem as margens da pokedex;
    -Pokedex limitada ao id:1008 por falta de dados.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const btnNext = document.querySelector('.btnNext');
 
 let pokeSearch = 1;
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
     console.log(APIResponse)
     if (APIResponse.status === 200) {
         const data = await APIResponse.json();
