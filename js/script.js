812b8883ffb11445fb580dc949dd318af82a7f2f
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-19 13:41:21 -0300

    Novidades e correções de bugs!!
    - Novo botão "SHINY", com ele você poderá ver a versão com cores "alternativas" do Pokémon escolhido;
    - Correção de bugs relacionados aos Pokémon shiny;
    - A base de dados só tem Pokémon shiny até o id: 905.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const pokeForm = document.querySelector('.pokeForm');
 const pokeInput = document.querySelector('.inputSearch');
 const btnPrev = document.querySelector('.btnPrev');
 const btnNext = document.querySelector('.btnNext');
+const btnShiny = document.querySelector('.btnShiny');
 
 let pokeSearch = 1;
 const fetchPokemon = async (pokemon) => {
@@ -30,7 +31,7 @@ const renderPokemon = async (pokemon) => {
         pokeImg.style.bottom = '55%'
         if (data.id < 650) {
             pokeImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-        }  else if (data.id < 1009) {
+        } else if (data.id < 1009) {
             pokeImg.src = data['sprites']['front_default'];
             pokeImg.style.height = '24%'
             pokeImg.style.bottom = '52.5%'
@@ -47,6 +48,23 @@ const renderPokemon = async (pokemon) => {
         pokeName.innerHTML = 'Not found :(';
     }
 };
+
+
+btnShiny.addEventListener('click', async () => {
+    const data = await fetchPokemon(pokeSearch);
+    pokeNum.innerHTML = data.id;
+    if (data.id < 650) {
+        pokeImg.style.height = '18%'
+        pokeImg.style.bottom = '55%'
+        pokeImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
+    } else if (data.id < 906) {
+        pokeImg.src = data['sprites']['front_shiny'];
+        pokeImg.style.height = '24%'
+        pokeImg.style.bottom = '52.5%'
+    }else{
+        alert('ERRO 404 - NÃO TEMOS POKEMONS SHINY EM NOSSA BASE DE DADOS PÓS ID:905')
+    }
+});
 
 pokeForm.addEventListener('submit', (event) => {
     event.preventDefault();
